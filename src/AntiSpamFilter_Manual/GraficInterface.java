9e8299326bff237699f7a5ce5013e9400b2125fa
Adaizilson Amado <anaoo@iscte-iul.pt>
2017-12-02 16:03:40 +0000

    Implementação gráfica da Interface: implementação dos botões, das textareas, e dos textFields

--- src/AntiSpamFilter_Manual/GraficInterface.java
+++ src/AntiSpamFilter_Manual/GraficInterface.java
@@ -6,14 +6,11 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.text.JTextComponent;
 
 public class GraficInterface {
 	
@@ -38,6 +35,73 @@ public class GraficInterface {
 			rulesTextField = new JTextField();
 			rulesTextField.setEditable(true);
 			rulesTextField.setPreferredSize(new Dimension(50, 30));
+			JPanel gridPanelForTexts = new JPanel(new GridLayout(3, 1));
+			gridPanelForTexts.add(rulesTextField);
+			gridPanelForTexts.add(hamTextField);
+			gridPanelForTexts.add(spamTextField);
+
+			pathToSpamButton = new JButton("Open Spam file");
+			pathToHamButton = new JButton("Open Ham file");
+			pathToRulesButton = new JButton("Open Rules file");
+			pathToHamButton.setPreferredSize(new Dimension(10, 30));
+
+			JPanel gridPanelForButtons = new JPanel(new GridLayout(3, 1));
+			gridPanelForButtons.add(pathToRulesButton);
+
+			gridPanelForButtons.add(pathToHamButton);
+			gridPanelForButtons.add(pathToSpamButton);
+
+			JPanel linkAndButton = new JPanel(new BorderLayout());
+			linkAndButton.add(gridPanelForTexts, BorderLayout.CENTER);
+			linkAndButton.add(gridPanelForButtons, BorderLayout.EAST);
+
+			areaForLink = new JTextArea();
+			areaForLink.setEditable(false);
+
+			areaForLink.setBackground(Color.WHITE);
+
+			JScrollPane scroll1 = new JScrollPane(areaForLink);
+			scroll1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+			scroll1.setPreferredSize(new Dimension(350, 300));
+
+			areaForWeight = new JTextArea();
+			areaForWeight.setFocusable(true);
+			areaForWeight.setBackground(Color.WHITE);
+
+			JScrollPane scroll2 = new JScrollPane(areaForWeight);
+			scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+			scroll2.setPreferredSize(new Dimension(50, 300));
+
+			JPanel panelForTextArea = new JPanel();
+			panelForTextArea.setSize(200, 200);
+			panelForTextArea.add(scroll1, BorderLayout.WEST);
+			panelForTextArea.add(scroll2, BorderLayout.EAST);
+
+			buttonValidar = new JButton("Validar");
+			buttonGravar = new JButton("Gravar");
+			buttonGravar.setPreferredSize(new Dimension(75, 25));
+			buttonValidar.setPreferredSize(new Dimension(75, 25));
+
+			JPanel p = new JPanel();
+			p.add(buttonValidar, BorderLayout.NORTH);
+			p.add(buttonGravar, BorderLayout.SOUTH);
+
+			JPanel eastPanel = new JPanel();
+			eastPanel.add(p, BorderLayout.CENTER);
+
+			JPanel menu = new JPanel(new BorderLayout());
+			menu.add(linkAndButton, BorderLayout.NORTH);
+			menu.add(panelForTextArea, BorderLayout.CENTER);
+
+			janela.setSize(600, 500);
+			janela.setResizable(false);
+			janela.setLocation(500, 300);
+			janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			janela.add(menu);
+			janela.add(eastPanel, BorderLayout.SOUTH);
+			janela.setVisible(true);
+
+		
 
 		}
 
