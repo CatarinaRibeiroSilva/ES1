21221e781a861507e1921d0dbc95e4b6aa8e00a0
Adaizilson Amado <anaoo@iscte-iul.pt>
2017-12-02 16:21:59 +0000

    Implementação do código que faz a leitura das rules

--- src/AntiSpamFilter_Manual/ReadRules.java
+++ src/AntiSpamFilter_Manual/ReadRules.java
@@ -1,9 +1,65 @@
 package AntiSpamFilter_Manual;
 
-public class ReadRules {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
-	public ReadRules() {
-	//codigo por desenvolver
+public class ReadRules {
+	private Random random = new Random();
+	private String rulesPath = " ";
+
+	private HashMap<String, Integer> rulesMap;
+
+	private FileReader fileReader;
+
+	public ReadRules(String rulesPath) {
+		setRulesPath(rulesPath);
+
+	}
+
+	public HashMap<String, Integer> Read(String path) {
+		ArrayList<String> listaRules = new ArrayList<>();
+		rulesMap = new HashMap<String, Integer>();
+
+		String line = null;
+
+		try {
+
+			try {
+				fileReader = new FileReader(path);
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}
+
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] tokens = line.split("	");
+				listaRules.add(tokens[0]);
+				int weigth = random.nextInt(5 + 1 + 5) - 5;
+				rulesMap.put(tokens[0], weigth);
+			}
+
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '");
+
 		}
+		return rulesMap;
+	}
+
+	public String getRulesPath() {
+		return rulesPath;
+	}
+
+	public void setRulesPath(String rulesPath) {
+		this.rulesPath = rulesPath;
+	}
 
 }
