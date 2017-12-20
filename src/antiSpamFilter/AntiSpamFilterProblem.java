abb68634010ed7611729e8eb462026f646173e94
kandrade <kabae@iscte-iul.pt>
2017-12-20 16:57:32 +0000

    Alteração da classes AntiSpaProblem para implementação da parte automática.

--- src/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/antiSpamFilter/AntiSpamFilterProblem.java
@@ -10,37 +10,44 @@ import AntiSpamFilter_Manual.ReadMessages;
 
 public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
-	  public AntiSpamFilterProblem() {
-	    // 335 variables (anti-spam filter rules) by default 
-	    this(335);
-	  }
-
-	  public AntiSpamFilterProblem(Integer numberOfVariables) {
-	    setNumberOfVariables(numberOfVariables);
-	    setNumberOfObjectives(2);
-	    setName("AntiSpamFilterProblem");
-
-	    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
-	    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
-
-	    for (int i = 0; i < getNumberOfVariables(); i++) {
-	      lowerLimit.add(-5.0);
-	      upperLimit.add(5.0);
-	    }
-
-	    setLowerLimit(lowerLimit);
-	    setUpperLimit(upperLimit);
-	  }
-
-	  public void evaluate(DoubleSolution solution){
-	 
-	    double[] x = new double[getNumberOfVariables()];
-	    for (int i = 0; i < solution.getNumberOfVariables(); i++) {
-	      x[i] = solution.getVariableValue(i) ;
-	    }
-
-	    ReadMessages rr = new ReadMessages();
-	    
-
-	  }
+	public AntiSpamFilterProblem() {
+		// 335 variables (anti-spam filter rules) by default
+		this(335);
 	}
+
+	public AntiSpamFilterProblem(Integer numberOfVariables) {
+		setNumberOfVariables(numberOfVariables);
+		setNumberOfObjectives(2);
+		setName("AntiSpamFilterProblem");
+
+		List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables());
+		List<Double> upperLimit = new ArrayList<>(getNumberOfVariables());
+
+		for (int i = 0; i < getNumberOfVariables(); i++) {
+			lowerLimit.add(-5.0);
+			upperLimit.add(5.0);
+		}
+
+		setLowerLimit(lowerLimit);
+		setUpperLimit(upperLimit);
+	}
+
+	public void evaluate(DoubleSolution solution) {
+
+		double[] x = new double[getNumberOfVariables()];
+		for (int i = 0; i < solution.getNumberOfVariables(); i++) {
+			x[i] = solution.getVariableValue(i);
+		}
+
+		ReadMessages readMessages = new ReadMessages();
+
+		// usar só depois de mudar a estrutura do calculo do FP e FN
+		// readMessages.calcularFN();
+		// readMessages.calcularFP();
+		//
+		// int fp=get();
+		// int fn= get();
+		// solution.setObjective(0, fp);
+		// solution.setObjective(1, fn);
+	}
+}
